7ff99084ec872bbf4b87be4e93c91cf096103915
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2020-06-02 12:49:18 +0200

    Use setuptools

--- march_rqt_gait_selection/setup.py
+++ march_rqt_gait_selection/setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-from distutils.core import setup
-
 from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
 
 d = generate_distutils_setup(
     packages=['march_rqt_gait_selection'],
--- march_rqt_software_check/setup.py
+++ march_rqt_software_check/setup.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-from distutils.core import setup
-
 from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
 
 d = generate_distutils_setup(
     packages=['march_rqt_software_check', 'march_rqt_software_check.checks'],
     package_dir={'': 'src'},
-    scripts=['scripts/march_rqt_software_check'],
+    scripts=['scripts/march_git_branch_check', 'scripts/march_rqt_software_check'],
 )
 
 setup(**d)
